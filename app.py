ceaea5958f8cf2883f75be79a66e04819770b842
stealthycoin <john.w.carlyle@gmail.com>
2017-07-19 23:26:51 -0700

    implements #2

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ app = Chalice(app_name='query-args-as-a-service')
 @app.route('/query-string-pls', methods=['POST'])
 def query_string():
     return urlencode(app.current_request.json_body)
+
+
+@app.route('/query-2-query')
+def query_2_query():
+    return urlencode(app.current_request.query_params)
